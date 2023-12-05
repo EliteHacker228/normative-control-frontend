581cbbaf52a69e8fcfc1ea376c7b90ef2c068b86
EliteHacker228 <mchappycraft@yandex.ru>
2023-12-05 23:34:18 +0500

    Add header and welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import viteLogo from '/vite.svg'
 import css from './App.module.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Default from "./pages/default/Default.jsx";
+import Welcome from "./pages/welcome/Welcome.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path='' element={<Default/>}/>
-          <Route path='/aboba' element={<Default/>}/>
+          {/*<Route path='' element={<Default/>}/>*/}
+          <Route path='' element={<Welcome/>}/>
+          <Route path='/welcome' element={<Welcome/>}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,22 @@
+import css from './Header.module.css';
+import normative_control_logo from './normative_control_logo.svg';
+import dropdown_icon from './dropdown_arrow.svg';
+
+export default function Header() {
+    return (
+        <div className={css.header}>
+            <div className={css.logoBlock}>
+                <img className={css.logoBlock_image} src={normative_control_logo}></img>
+                <p className={css.logoBlock_text}>normative control</p>
+            </div>
+
+            <div className={css.controlBlock}>
+                <p className={css.header_text}>FAQ</p>
+                <div className={css.loginBlock}>
+                    <p className={css.header_text}>Войти</p>
+                    <img src={dropdown_icon} alt={'Dropdown arrow'}/>
+                </div>
+            </div>
+        </div>
+    );
+}
