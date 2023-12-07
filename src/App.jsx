2e69ba604ec9211fd2f930a1db257627490dab5c
EliteHacker228 <mchappycraft@yandex.ru>
2023-12-08 03:05:30 +0500

    Add uploading page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from '/react.svg'
 import viteLogo from '/vite.svg'
 import css from './App.module.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Default from "./pages/default/Default.jsx";
 import Welcome from "./pages/welcome/Welcome.jsx";
-import Faq from "./pages/faq/Faq.jsx";
+import Uploading from "./pages/uploading/Uploading.jsx";
 import Result from "./pages/result/Result.jsx";
+import Faq from "./pages/faq/Faq.jsx";
+
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-          {/*<Route path='' element={<Default/>}/>*/}
-          <Route path='' element={<Welcome/>}/>
-          <Route path='/welcome' element={<Welcome/>}/>
-          <Route path='/result' element={<Result/>}/>
-          <Route path='/faq' element={<Faq/>}/>
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/*<Route path='' element={<Default/>}/>*/}
+                <Route path='' element={<Welcome/>}/>
+                <Route path='/welcome' element={<Welcome/>}/>
+                <Route path='/uploading' element={<Uploading/>}/>
+                <Route path='/result' element={<Result/>}/>
+                <Route path='/faq' element={<Faq/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
---
+++ src/pages/uploading/Uploading.jsx
@@ -0,0 +1,64 @@
+import {useEffect, useRef, useState} from "react";
+import css from './Uploading.module.css';
+import sleepyPandaImg from './sleepy_panda_img.png';
+import awakePandaImg from './awake_panda_img.png';
+import uploadingComplete from './progress_bar_ready_img.png';
+import Header from "../../components/header/Header.jsx";
+
+export default function Uploading() {
+    const [uploadingProgress, setUploadingProgress] = useState(0);
+    const progressWheel = useRef(null);
+
+    let timer;
+    const updateCount = () => {
+        timer = setInterval(() => {
+            progressWheel.current.style.background = `conic-gradient(blue ${(uploadingProgress + 1) * 3.6}deg, white 0deg)`;
+            console.log(progressWheel.current);
+            // progressTitle.current.textContent = `Ожидание${'.'.repeat(uploadingProgress % 3)}`;
+            setUploadingProgress(prevCount => prevCount + 1);
+        }, 10)
+
+        if (uploadingProgress === 100) {
+            clearInterval(timer);
+        }
+    }
+    useEffect(() => {
+        updateCount();
+        return () => clearInterval(timer);
+    }, [uploadingProgress]);
+
+    return (
+        <>
+            <Header/>
+            {uploadingProgress === 100 ?
+                <div>
+                    <div className={css.contentWrap}>
+                        <div className={css.content}>
+                            <p className={css.progressStatus}></p>
+                            <div className={css.contentCenter}>
+                                <div className={css.finishBanner}>
+                                    <img className={css.uploadingCompleteImg} src={uploadingComplete} alt={'Uploading complete'}/>
+                                    <p className={css.progressStatus}>Проверка окончена!</p>
+                                </div>
+                                <button className={css.uploadButton}>ПОСМОТРЕТЬ РЕЗУЛЬТАТ</button>
+                                <img className={css.sleepyPandaImg} src={sleepyPandaImg}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                :
+                <div className={css.contentWrap}>
+                    <div className={css.content}>
+                        <div className={css.contentCenter}>
+                            <div className={css.progressWheel} ref={progressWheel}>
+                                <p className={css.progressTitle}>{`${uploadingProgress}%`}</p>
+                            </div>
+                            <p className={css.progressStatus}>Ожидание...</p>
+                        </div>
+                        <img className={css.sleepyPandaImg} src={sleepyPandaImg}/>
+                    </div>
+                </div>
+            }
+        </>
+    );
+}
