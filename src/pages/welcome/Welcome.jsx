a387920a35cfdaaa1801f0e0fcf0831cd95fe309
EliteHacker228 <mchappycraft@yandex.ru>
2023-12-15 01:25:50 +0500

    Refactoring of welcome page

--- src/pages/welcome/Welcome.jsx
+++ src/pages/welcome/Welcome.jsx
@@ -6,21 +6,19 @@ export default function Welcome() {
     return (
         <div>
             <Header/>
-            <div className={css.contentWrap}>
-                <div className={css.content}>
-                    <div className={css.contentLeft}>
-                        <h1 className={css.contentH1}>Проверка вашей выпускной квалифицированной работы
-                            на наличие ошибок оформления документа</h1>
-                        <p className={css.contentP}>Сервис поддерживает файлы формата <b>docx</b> объемом
-                            до <b>20МБ.</b></p>
-                        <div className={css.uploadBlock}>
-                            <button className={css.uploadButton}>ВЫБРАТЬ ФАЙЛ</button>
-                            <p className={css.uploadHint}>или перетащите файл сюда</p>
-                        </div>
+            <div className={css.content}>
+                <div className={css.contentLeft}>
+                    <h1 className={css.contentH1}>Проверка вашей выпускной квалифицированной работы
+                        на наличие ошибок оформления документа</h1>
+                    <p className={css.contentP}>Сервис поддерживает файлы формата <b>docx</b> объемом
+                        до <b>20МБ.</b></p>
+                    <div className={css.uploadBlock}>
+                        <button className={css.uploadButton}>ВЫБРАТЬ ФАЙЛ</button>
+                        <p className={css.uploadHint}>или перетащите файл сюда</p>
                     </div>
-                    <div className={css.contentRight}>
-                        <img className={css.contentImage} src={welcome_panda_img} alt={'Panda is welcoming user'}/>
-                    </div>
+                </div>
+                <div className={css.contentRight}>
+                    <img className={css.contentImage} src={welcome_panda_img} alt={'Panda is welcoming user'}/>
                 </div>
             </div>
         </div>
