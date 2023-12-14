b8a123e5c2f4eb9b2690f0fea9f3cebb8827d9e6
EliteHacker228 <mchappycraft@yandex.ru>
2023-12-14 17:51:32 +0500

    Add navigation links in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,27 @@
 import css from './Header.module.css';
 import normative_control_logo from './normative_control_logo.svg';
 import dropdown_icon from './dropdown_arrow.svg';
+import {NavLink} from "react-router-dom";
 
 export default function Header() {
     return (
         <div className={css.header}>
             <div className={css.headerContent}>
-                <div className={css.logoBlock}>
+                <NavLink className={css.logoBlock} to={'/'}>
                     <img className={css.logoBlock_image} src={normative_control_logo}></img>
                     <p className={css.logoBlock_text}>normative control</p>
-                </div>
+                </NavLink>
 
                 <div className={css.controlBlock}>
-                    <p className={css.header_text}>FAQ</p>
-                    <div className={css.loginBlock}>
-                        <p className={css.header_text}>Войти</p>
-                        <img src={dropdown_icon} alt={'Dropdown arrow'}/>
-                    </div>
+                    <NavLink className={css.link} to={'/faq'}>
+                        <p className={css.header_text}>FAQ</p>
+                    </NavLink>
+                    <NavLink className={css.link}>
+                        <div className={css.loginBlock}>
+                            <p className={css.header_text}>Войти</p>
+                            <img src={dropdown_icon} alt={'Dropdown arrow'}/>
+                        </div>
+                    </NavLink>
                 </div>
             </div>
         </div>
