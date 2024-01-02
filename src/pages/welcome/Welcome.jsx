d03c32900f0fcba2b6c722b8506e53b2990ab5e6
EliteHacker228 <mchappycraft@yandex.ru>
2024-01-02 22:10:54 +0500

    Refactor Welcome page to BEM

--- src/pages/welcome/Welcome.jsx
+++ src/pages/welcome/Welcome.jsx
@@ -7,19 +7,18 @@ export default function Welcome() {
         <div>
             <Header/>
             <div className={css.content}>
-                <div className={css.contentLeft}>
-                    <h1 className={css.contentH1}>Проверка вашей выпускной квалифицированной работы
+                <div className={css.content__main}>
+                    <h1 className={css.content__header}>Проверка вашей выпускной квалифицированной работы
                         на наличие ошибок оформления документа</h1>
-                    <p className={css.contentP}>Сервис поддерживает файлы формата <b>docx</b> объемом
+                    <p className={css.content__text}>Сервис поддерживает файлы формата <b>docx</b> объемом
                         до <b>20МБ.</b></p>
-                    <div className={css.uploadBlock}>
-                        <button className={css.uploadButton}>ВЫБРАТЬ ФАЙЛ</button>
-                        <p className={css.uploadHint}>или перетащите файл сюда</p>
+                    <div className={css.content__upload}>
+                        <button className={`${css.button} ${css.button_upload} ${css.button_red} ${css.button_shadow}`}>ВЫБРАТЬ ФАЙЛ
+                        </button>
+                        <p className={css.content__hint}>или перетащите файл сюда</p>
                     </div>
                 </div>
-                <div className={css.contentRight}>
-                    <img className={css.contentImage} src={welcome_panda_img} alt={'Panda is welcoming user'}/>
-                </div>
+                <img className={css.content__image} src={welcome_panda_img} alt={'Panda is welcoming user'}/>
             </div>
         </div>
     );
