877fb6ac71d54797ee44fc51593b4dcf8846972e
EliteHacker228 <mchappycraft@yandex.ru>
2024-01-17 09:30:08 +0500

    Add drag and drop file uploading on Welcome page

--- src/pages/welcome/Welcome.jsx
+++ src/pages/welcome/Welcome.jsx
@@ -15,9 +15,14 @@ export default function Welcome() {
     const [fileError, setFileError] = useState(null);
 
     let onFileUpload = (e) => {
+        e.preventDefault();
+        let file = e.target.files[0];
+        handleFileUploading(file);
+    };
+
+    let handleFileUploading = (file) => {
         const _20MB = 20_971_520;
 
-        let file = e.target.files[0];
         let fileSize = file.size;
         let fileExtension = file.name.split('.').at(-1);
 
@@ -34,6 +39,30 @@ export default function Welcome() {
         navigate('/uploading');
     };
 
+    const [isDragActive, setIsDragActive] = useState(false);
+
+    const onDragEnter = (e) => {
+        e.preventDefault();
+        setIsDragActive(true);
+    };
+
+    const onDragLeave = (e) => {
+        e.preventDefault();
+        setIsDragActive(false);
+    };
+
+    const onDragOver = (e) => {
+        e.preventDefault();
+        setIsDragActive(true);
+    };
+
+    const onDrop = (e) => {
+        e.preventDefault();
+        setIsDragActive(false);
+        let file = e.dataTransfer.files[0];
+        handleFileUploading(file);
+    };
+
     return (
         <div>
             <Header/>
@@ -43,11 +72,24 @@ export default function Welcome() {
                         на наличие ошибок оформления документа</h1>
                     <p className={css.content__text}>Сервис поддерживает файлы формата <b>docx</b> объемом
                         до <b>20МБ.</b></p>
-                    <div className={css.content__upload}>
-                        <button className={`${css.button} ${css.button_upload} ${css.button_red} ${css.button_shadow}`}
-                                onClick={onUploadButtonClick}>ВЫБРАТЬ ФАЙЛ
-                        </button>
-                        <p className={css.content__hint}>или перетащите файл сюда</p>
+                    <div className={css.content__upload}
+                         onDragEnter={onDragEnter}
+                         onDragLeave={onDragLeave}
+                         onDragOver={onDragOver}
+                         onDrop={onDrop}>
+                        {isDragActive ?
+                            <div className={css.upload__dragndrop}>
+                                <p className={`${css.content__hint} ${css.content__hint__dragndrop}`}>Отпустите файл, чтобы начать загрузку</p>
+                            </div>
+                            :
+                            <div className={css.upload__controls}>
+                                <button
+                                    className={`${css.button} ${css.button_upload} ${css.button_red} ${css.button_shadow}`}
+                                    onClick={onUploadButtonClick}>ВЫБРАТЬ ФАЙЛ
+                                </button>
+                                <p className={css.content__hint}>или перетащите файл сюда</p>
+                            </div>
+                        }
                     </div>
                     {fileError === fileErrors.wrongFileExtension &&
                         <p className={`${css.content__hint} ${css.content__hint__error}`}>Неподдерживаемый формат.
