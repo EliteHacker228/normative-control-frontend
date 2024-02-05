89515438b0d5e346a95c3702a8783bbdc0443535
EliteHacker228 <mchappycraft@yandex.ru>
2024-02-06 02:41:03 +0500

    Fix form input's erasures on login/register windows changing in auth pop-up

--- src/components/header/authPopUp/AuthPopUp.jsx
+++ src/components/header/authPopUp/AuthPopUp.jsx
@@ -1,9 +1,16 @@
 import Login from "./login/Login.jsx";
 import Registration from "./registration/Registration.jsx";
+import css from './AuthPopUp.module.css';
 
 export default function AuthPopUp({selectedAuthType}) {
-    if (selectedAuthType === 'login')
-        return <Login/>;
-    else
-        return <Registration/>;
+    return (
+        <div>
+            <div className={selectedAuthType === 'login' ? '' : css.block__no_display}>
+                <Login/>
+            </div>
+            <div className={selectedAuthType === 'registration' ? '' : css.block__no_display}>
+                <Registration/>
+            </div>
+        </div>
+    );
 }
