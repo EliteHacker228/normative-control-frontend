8a5d927cb9abfb2feb7c9998258875e087eba7d3
EliteHacker228 <mchappycraft@yandex.ru>
2024-05-05 23:44:36 +0500

    Fix add feedback link to footer and fix it's display

--- src/actual/commonComponents/footer/Footer.jsx
+++ src/actual/commonComponents/footer/Footer.jsx
@@ -4,7 +4,12 @@ export default function Footer() {
     return (
         <div className={css.footer}>
             <div className={css.footer__content}>
-                <p className={css.footer__element}>© ИИТ ИНФО «УрФУ имени первого Президента России Б.Н. Ельцина»</p>
+                <p className={css.footer__element__left}>Данная версия является тестовой и не отражает качество финальной
+                    версии сервиса.
+                    Пройдите <a className={css.feedback} rel="noopener noreferrer" target="_blank"
+                                href={'https://forms.gle/hewHJgWNdH91eakYA'}>короткий опрос</a>, чтобы помочь нам
+                    улучшить качество финального продукта.</p>
+                <p className={css.footer__element__right}>© ИИТ ИНФО «УрФУ имени первого Президента России Б.Н. Ельцина»</p>
             </div>
         </div>
     );
