cdb1b9ee63a5f22f5737ca72bae250e4b09b7b80
EliteHacker228 <mchappycraft@yandex.ru>
2024-06-05 06:59:34 +0500

    Add academical groups page layout

--- src/actual/pages/admin/profile/academicalGroups/AcademicalGroups.jsx
+++ src/actual/pages/admin/profile/academicalGroups/AcademicalGroups.jsx
@@ -1,12 +1,62 @@
 import Header from "../../../../commonComponents/header/Header.jsx";
 import Footer from "../../../../commonComponents/footer/Footer.jsx";
+import {useEffect, useState} from "react";
+import AcademicalGroupsService from "../../../../services/AcademicalGroupsService.js";
+import css from "./AcademicalGroups.module.css";
+import searchIco from './static/search_ico.svg';
+import whiteDownloadIco from './static/downloadIcoWhite.svg';
 
-export default function AcademicalGroups(){
+
+export default function AcademicalGroups() {
+    const [academicalGroups, setAcademicalGroups] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            let academicalGroups = await AcademicalGroupsService.getAcademicalGroups();
+            setAcademicalGroups(academicalGroups);
+        })();
+    }, []);
+
     return (
         <div>
             <Header/>
-            <div>
-                Academical groups
+            <div className={css.academicalGroups}>
+                <h1 className={css.academicalGroups__header}>Академические группы</h1>
+                <div className={css.search}>
+                    <div className={css.searchHeader}>
+                        <img className={css.searchHeader__ico} src={searchIco} alt='Иконка поиска' title='Поиск'/>
+                        <h1 className={css.searchHeader__text}>Поиск</h1>
+                    </div>
+                    <div className={css.searchInput}>
+                        <input className={css.searchInput__field} type='text' placeholder={'Группа'}/>
+                    </div>
+                    <div className={css.searchResults}>
+                        {academicalGroups.map((academicalGroup, index) => {
+                                return <div key={index} className={css.searchResult}>
+                                    <div className={css.searchResult__text}>{academicalGroup.name}</div>
+                                    <div className={css.searchResult__controls}>
+                                        <button
+                                            className={`${css.search__button} ${css.search__button_open}`}>Открыть
+                                        </button>
+                                        <button
+                                            className={`${css.search__button} ${css.search__button_edit}`}>Изменить
+                                        </button>
+                                        <button
+                                            className={`${css.search__button} ${css.search__button_remove}`}>Удалить
+                                        </button>
+                                    </div>
+                                </div>;
+                            }
+                        )}
+                    </div>
+                    <div className={css.search__controls}>
+                        <button className={`${css.search__button} ${css.search__button_add}`}>Добавить</button>
+                        <div className={`${css.search__button} ${css.search__button_download}`}>
+                            <p>Скачать список всех работ</p>
+                            <img className={css.downloadImage} src={whiteDownloadIco} alt={'Кнопка скачивания списка работ'} title={'Скачать'}/>
+                        </div>
+                    </div>
+                </div>
             </div>
             <Footer/>
         </div>
