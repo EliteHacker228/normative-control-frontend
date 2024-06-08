2a7287da25a024d95b39d4be8749b73a5c087da1
EliteHacker228 <mchappycraft@yandex.ru>
2024-06-08 05:22:53 +0500

    Change academical groups forms validation

--- src/actual/utils/Validators/AcademicalGroupValidator.js
+++ src/actual/utils/Validators/AcademicalGroupValidator.js
@@ -1,11 +1,15 @@
 export default class AcademicalGroupValidator {
     static isAcademicalGroupCreationFormValid(academicalGroupName) {
+        if(academicalGroupName.startsWith(' ') || academicalGroupName.endsWith(' '))
+            return false;
         if (academicalGroupName === '')
             return false;
         return true;
     }
 
     static isAcademicalGroupEditionFormValid(academicalGroupName, normocontrollerId, initialAcademicalGroupName, initialNormocontrollerId) {
+        if(academicalGroupName.startsWith(' ') || academicalGroupName.endsWith(' '))
+            return false;
         if (academicalGroupName === '')
             return false;
         if (+normocontrollerId === +initialNormocontrollerId && academicalGroupName === initialAcademicalGroupName)
